037c5a8100a64a297332f3ad92dec2f725e20c6a
Liam Shaun Smith <liam@liamshaunsmith.dev>
2025-09-27 13:04:01 +0100

    Prepare repository for task

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App.js";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import { App } from './components/App.tsx';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  console.error('Unable to find root element.');
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
